717b98059067929d63665c9b3a6dc6d9abfc6c20
Kim Tae-wook <kteem84@gmail.com>
2020-02-24 16:40:50 +0900

    add a toolchain using ndk r21 and api 24

--- bin/detail/toolchain_table.py
+++ bin/detail/toolchain_table.py
@@ -177,6 +177,7 @@ toolchain_table = [
     Toolchain('android-ndk-r18b-api-21-armeabi-v7a-clang-libcxx', 'Unix Makefiles'),
     Toolchain('android-ndk-r18b-api-21-x86-64-clang-libcxx', 'Unix Makefiles'),
     Toolchain('android-ndk-r18b-api-21-x86-clang-libcxx', 'Unix Makefiles'),
+    Toolchain('android-ndk-r21-api-24-arm64-v8a-clang-libcxx11', 'Unix Makefiles'),
     Toolchain('emscripten-cxx11', 'Unix Makefiles'),
     Toolchain('emscripten-cxx14', 'Unix Makefiles'),
     Toolchain('emscripten-cxx17', 'Unix Makefiles'),
